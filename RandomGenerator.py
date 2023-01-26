4b45170ed339e2b0b744e0aaf79dcdfe38404c0f
NubScrub <12957416+NubScrub@users.noreply.github.com>
2023-01-26 23:44:58 +0000

    Create RandomGenerator.py

---
+++ RandomGenerator.py
@@ -0,0 +1,25 @@
+# This code uses the Datamuse API (https://www.datamuse.com/api/) to search for related words 
+# based on the keyword input by the user. It then selects a random word from the search results 
+# and combines it with the keyword to create a random business name. In this example, it is using "Co" 
+# as the company's name, you can change it as per your requirement.
+
+import random
+import requests
+
+def generate_name(keyword):
+    # Use the keyword to search for related words on the internet
+    search_url = "https://api.datamuse.com/words?rel_jjb=" + keyword
+    search_results = requests.get(search_url).json()
+    
+    # Choose a random word from the search results
+    adj = random.choice(search_results)['word']
+    
+    # Use the keyword and chosen word to create a random business name
+    name = adj + " " + keyword + " " + "Co"
+    return name
+
+# Get input from the user
+keyword = input("Enter a keyword for your business name: ")
+
+# Generate and print the business name
+print(generate_name(keyword))   
