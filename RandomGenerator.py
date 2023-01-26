3a28e7b640257b23bc9d4057fe8bc83a600162fe
NubScrub <12957416+NubScrub@users.noreply.github.com>
2023-01-26 23:53:09 +0000

    Update RandomGenerator.py
    Code now uses the input method to get the number of results user wants to generate, and it uses a while loop with a menu interaction to allow the user to generate new lists of business names with different keywords as many times as they want.

--- RandomGenerator.py
+++ RandomGenerator.py
@@ -6,7 +6,7 @@
 import random
 import requests
 
-def generate_name(keywords):
+def generate_name(keywords, num_results):
     # Use the keyword to search for related words on the internet
     keywords_list = keywords.split(",")
     search_results = []
@@ -14,15 +14,37 @@ def generate_name(keywords):
         search_url = "https://api.datamuse.com/words?rel_jjb=" + keyword
         search_results += requests.get(search_url).json()
     
-    # Choose a random word from the search results
-    adj = random.choice(search_results)['word']
-    
-    # Use the keyword and chosen word to create a random business name
-    name = adj + " " + keyword + " " + "Co"
-    return name
+    # Create a list to store the generated names
+    names_list = []
+    for i in range(num_results):
+        # Choose a random word from the search results
+        adj = random.choice(search_results)['word']
+        # Use the keyword and chosen word to create a random business name
+        name = adj + " " + keyword + " " + "Co"
+        names_list.append(name)
+    return names_list
 
 # Get input from the user
 keywords = input("Enter a comma separated keywords for your business name: ")
+num_results = int(input("Enter the number of results you want to generate: "))
 
-# Generate and print the business name
-print(generate_name(keywords))
+# Generate and print the business names
+names_list = generate_name(keywords, num_results)
+print("Generated Business names:")
+for name in names_list:
+    print(name)
+
+# Menu interaction to generate new list
+while True:
+    regenerate = input("Do you want to generate new names? (y/n): ")
+    if regenerate.lower() == "y":
+        keywords = input("Enter a comma separated keywords for your business name: ")
+        num_results = int(input("Enter the number of results you want to generate: "))
+        names_list = generate_name(keywords, num_results)
+        print("Generated Business names:")
+        for name in names_list:
+            print(name)
+    elif regenerate.lower() == "n":
+        break
+    else:
+        print("Invalid input. Please enter y or n.")
